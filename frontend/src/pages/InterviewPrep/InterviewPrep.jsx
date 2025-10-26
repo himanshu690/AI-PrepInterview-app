f0c1afab3b3e0518d3209243f590a13f1bf12ed6
Himanshu Rajput <aditya84961@gmail.com>
2025-10-26 16:47:30 +0530

    description of question

--- frontend/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -1,10 +1,119 @@
-import React from 'react'
-
+import moment from 'moment'
+import React, { useEffect, useState } from 'react'
+import { LuCircleAlert, LuListCollapse } from 'react-icons/lu'
+import { useParams } from 'react-router-dom'
+import SpinnerLoader from '../../components/Loader/SpinnerLoader'
+import toast from 'react-hot-toast'
+import { AnimatePresence, motion } from "framer-motion";
+import DashboardLayout from '../../components/layouts/DashboardLayout'
+import RoleInfoHeader from './RoleInfoHeader'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPath'
+import QuestionCard from '../../components/Cards/QuestionCard'
 const InterviewPrep = () => {
+  const {sessionId} = useParams();
+  const [sessionData, setSessionData] = useState(null);
+  const [erroMsg, setErrorMsg] = useState("");
+
+  const [openLearnMoreDrawer, setOpenLearnMoreDrawer] = useState(false);
+  const [explanation, setExplanation]= useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUpdateLoader, setUpdateLoader] = useState(false);
+
+  const fetchSessionDetailsById = async() =>{
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.SESSION.GET_ONE(sessionId)
+      );
+
+      if(response.data && response.data.session){
+        setSessionData(response.data.session)
+      }
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  }
+
+  const generateConceptExplanation = async(question) =>{
+
+  }
+  const toggleQuestionPinStatus = async(questionId) =>{
+
+  }
+  const uploadMoreQuestions = async(question) =>{
+
+  }
+
+  useEffect(()=>{
+    if(sessionId){
+      fetchSessionDetailsById()
+    }
+  }, []);
+
   return (
-    <div>
-      Interview
-    </div>
+    <DashboardLayout>
+      <RoleInfoHeader
+        role={sessionData?.role || ""}
+        topicsToFocus={sessionData?.topicsToFocus || ""}
+        experience={sessionData?.experience || ""}
+        description={sessionData?.description || ""}
+        questions={sessionData?.questions.length || ""}
+        lastUpdated ={
+          sessionData?.updatedAt
+            ? moment(sessionData.updatedAt).format("Do MMM YYYY")
+            : ""
+        }
+      />
+
+      <div className='container mx-auto pt-4 pb-4 px-4 md:px-0'>
+        <h2 className='text-lg font-semibold color-black'>Interview Q & A</h2>
+        <div className='grid grid-cols-12 gap-4 mt-5 mb-10'>
+          <div className={`
+            col-span-12 ${
+              openLearnMoreDrawer ? "md:col-span-7" : "md:col-span-8"
+            }
+            `}
+          >
+            <AnimatePresence>
+              {sessionData?.questions?.map((data, index)=>{
+                return (
+                  <motion.div
+                    key={data._id || index}
+                    initial ={{opacity: 0, y:-20}}
+                    animate={{opacity:1, y:0}}
+                    exit={{opacity:0, scale: 0.95}}
+                    transition={{
+                      duration:0.4,
+                      type:"string",
+                      stiffness:100,
+                      delay: index * 0.1,
+                      damping: 15,
+                    }}
+                    layout
+                    layoutId={`question-${data._id || index}`}
+                  >
+                    <>
+                      <QuestionCard
+                        question={data?.question}
+                        answer={data?.answer}
+                        onLearnMore={()=>
+                          generateConceptExplanation(data.question)
+                        }
+                        isPinned = {data?.isPinned}
+                        onTogglePin={()=> toggleQuestionPinStatus(data._id)}
+                      />
+
+                    </>
+                  </motion.div>
+                )
+              })}
+            </AnimatePresence>
+          </div>
+        </div>
+      </div>
+
+    </DashboardLayout>
   )
 }
 
